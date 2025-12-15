4eec47636fe755802e3a8547a23847053be6f491
Gustavo Tamanini <gustavotamanini2005@gmail.com>
2025-12-15 20:49:19 -0300

    Fabricação dos carros

--- controller/processa.php
+++ controller/processa.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     require_once "../model/Fabrica.php";
     require_once "../model/Carro.php";
 
@@ -7,11 +7,61 @@
 
         switch($acao){
             case 'fabricar':
+                echo '<h2>🏭 Você escolheu fabricar carros!</h2>
+                <p>Quantos carros deseja fabricar?:</p>
+                        <form action="processa.php" method="post">
+                        <input type="hidden" name="acao" value="salvar_carros">
+
+                        <label><strong>Quantidade de Carros:</strong></label><br>
+                        <input type="number" name="qtde_carros" min="0" required><br><br>
+
+                        <button type="submit">Avançar</button>
+                    </form>
+                ';
                 break;
-            case 'salvar_carro':
+            case 'salvar_carros':
+                $qtde_carros = (int)$_POST['qtde_carros'] ?? 0;            
+                if($qtde_carros > 0){
+                    echo '<h2>🏭 Vamos fabricar ' . $qtde_carros+1 .' carros!</h2>';
+                    echo '<form action="processa.php" method="post">';
+                    for($i = 0; $i < $qtde_carros; $i++){
+                        echo "
+                            <input type='hidden' name='acao' value='finalizar_carros'>
+                            <input type='hidden' name='qtde_carros' value='{$qtde_carros}'>
+                            <input type='hidden' name='qtde_carros' value='{$qtde_carros}'>
+                            <input type='hidden' name='qtde_carros' value='{$qtde_carros}'>
+    
+                            <label><strong>Cor do " . $i+1 . "º Carro:</strong></label><br>
+                            <input type='text' name='cor_carro_{$i}' required><br><br>
+    
+                            <label><strong>Modelo do " . $i+1 . "º Carro:</strong></label><br>
+                            <input type='text' name='modelo_carro_{$i}' required><br><br><br>
+                    ";
+                    }
+                    echo '<button type="submit">Avançar</button>
+                    </form>';
+                }
                 break;
-            case 'finalizar_carro':
+            case 'finalizar_carros':
+                $qtde_carros = (int)$_POST['qtde_carros'] ?? 0;
+
+                $cor = [];
+                for($i = 0; $i < $qtde_carros; $i++){
+                    $cor[$i] =  $_POST["cor_carro_{$i}"] ?? '';
+                    //echo "<br>Armazenado a cor " . $cor[$i] . " no veículo " . $i+1;
+                }
+
+                $modelo = [];
+                for($i = 0; $i < $qtde_carros; $i++){
+                    $modelo[$i] = $_POST["modelo_carro_{$i}"] ?? '';
+                    //echo "<br>Armazenado o modelo " . $modelo[$i] . " no veículo " . $i+1;
+                }
                 break;
+
+                $fabrica = new Fabrica();
+                for($i = 0; $i < $qtde_carros; $i++){
+                    $fabrica->fabricarCarro($qtde_carros, $cor, $modelo);
+                }
             case 'vender':
                 break;
             case 'selecionar_carro':
--- model/Fabrica.php
+++ model/Fabrica.php
@@ -2,14 +2,21 @@
     require_once "Carro.php";
 
     class Fabrica{
-        private $carros = [];
+        private array $carros = [];
+
+        public function setCarros(object $carro): void{
+            array_push($this->carros, $carro);
+        }
         
         public function venderCarro(){
             //irá retirar um carro do array;
         }
 
-        public function fabricarCarro(int $qtdeCarros){
+        public function fabricarCarro(int $qtdeCarros, $cor, $modelo){
             //deverá acrescenter um (ou mais) carros ao array;
+            $carro = new Carro();
+            $carro->setCor($cor);
+            $carro->setModelo($modelo);
         }
 
         public function getInfoCarros($carros){
